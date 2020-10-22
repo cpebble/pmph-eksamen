379fae68127dc26d829b0de2d4078674b944e7bd
Christian Påbøl Jacobsen <cpcp800@auroragames.dk>
2020-10-22 15:59:16 +0200

    More skeleton

---
+++ src/helpers.cu
@@ -0,0 +1,13 @@
+#include <sys/time.h>
+#include <time.h> 
+// Will include helper scripts for time values, validations, etc
+#include "helpers.h"
+
+int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
+{
+    unsigned int resolution=1000000;
+    long int diff = (t2->tv_usec + resolution * t2->tv_sec) - (t1->tv_usec + resolution * t1->tv_sec);
+    result->tv_sec = diff / resolution;
+    result->tv_usec = diff % resolution;
+    return (diff<0);
+}
---
+++ src/helpers.h
@@ -0,0 +1,5 @@
+#include <sys/time.h>
+#include <time.h> 
+// Will include helper scripts for time values, validations, etc
+
+int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1);
