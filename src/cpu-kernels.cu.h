5037136ed206ff7722c81cedb3cf3f81aafc1928
Silja Maria Knudsen <smk@schultz.dk>
2020-10-24 23:02:42 +0200

    Implementering af seq_mkX, er ikke sikker på den er rigtig :(

--- src/cpu-kernels.cu.h
+++ src/cpu-kernels.cu.h
@@ -1,21 +1,78 @@
 // This contains the sequential C versions to validate
 #ifndef _CPU_KERNELS
 #define _CPU_KERNELS
+#include <math.h>
+#include <cmath>
 
-// Makes interpolation matrix
-void seq_mkX (int K, float f, float* X_out);
+// Makes interpolation matrix 
+//K is the number of rows, K = 2*k + 2 
+//N is the number of cols
+void seq_mkX (int K, int N, float f, float* X_out){
+        for (int i = 0; i < K; i++)
+        {
+                for (int ind = 0; ind < N; ind++)
+                {
+                        //Calculates the current index for X_out
+                        int index = K*i + ind; 
+                        if(i == 0){
+                                X_out[index] = 1; //1f32
+                        }
+                        else if(i == 1){
+                                X_out[index] = i;//r32 ind 
+                        }else{
+                                float f_t = (2*M_PI*ind*i)/f; //Ft(j)=2πjt/f.
+                                if (i % 2 == 0){
+                                       X_out[index] = sin(f_t);  
+                                }else{
+                                    X_out[index] = cos(f_t);
+                                }
+                        }
+                }
+                
+        }
+        
+}
 
 // Filtered Matrix - Matrix Multiplication
 // X * X^T
-void seq_mmMulFilt(float* X, float* y, float* X_sqr);
+void seq_mmMulFilt(float* X, float* y, float* X_sqr){
+
+}
 
 // We need to invert here
 // Allocate the [K][2K] array in here
 // X_inv is the output
-void seq_matInv (float* X_sqr, float X_inv, int K);
+void seq_matInv (float* X_sqr, float* X_inv, int n, int m){
+        for (int row = 0; row < row; row++)
+        {
+                for (int col = 0; col < m; col ++)
+                {
+                        float val = X_sqr[row*n + col];
+                        X_inv[col*m + row] = val;                                               
+                }
+        }
+}
+
+
+bool isnan(float y){
+
+}
 
 // Filtered Matrix * Vector multiplication
-void seq_mvMulFilt(float* X, float* y, float Beta_zero);
+void seq_mvMulFilt(float* X, float* y, float* Beta_zero, int n, int m){
+        for (int i = 0; i < n; i++)
+        {
+                Beta_zero[i] = 0; 
+                for (int j = 0; j < m; j++)
+                {
+                        int index_X = n*i + j;
+                        float cur_X = X[index_X]; 
+                        float cur_y = y[j]; 
+                        float xy = cur_X * cur_y * (1.0 - isnan(cur_y)); 
+                        Beta_zero[i] += xy; //er ikke sikker på at den skal indsættes der                         
+                }                
+        }       
+}
 
 // UnFiltered matrix * vector multiplication
 // Used for calculating Beta, and y_preds
