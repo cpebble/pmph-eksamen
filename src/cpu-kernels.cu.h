6285ae187e19d6e725627b5baa8b2fa36a0a32f2
Silja Maria Knudsen <smk@schultz.dk>
2020-10-26 14:58:32 +0100

    Rettet seq_mkX

--- src/cpu-kernels.cu.h
+++ src/cpu-kernels.cu.h
@@ -4,37 +4,45 @@
 #include <math.h>
 #include <cmath>
 
-// Makes interpolation matrix 
-//K is the number of rows, K = 2*k + 2 
-//N is the number of cols
-void seq_mkX (int K, int N, float f, float* X_out){
+//-- Creates the interpolation matrix ---
+// K is the number of rows
+// N is the number of columns 
+// f is the frequency of the observations
+// X_out is the resulting interpolation matrix, which has size KxN
+//      ecach column, x_t, will correspond to the data generated for a spefic date t
+void seq_mkX(int K, int N, float f, float* X_out){
+        //Loops through the rows
         for (int i = 0; i < K; i++)
         {
-                for (int ind = 0; ind < N; ind++)
-                {
+                //Loops through the indices in each row and creates the elements
+                for(int t = 0; t < N; t++){
                         //Calculates the current index for X_out
-                        int index = K*i + ind; 
+                        int cur_ind = K*i + t;
                         if(i == 0){
-                                X_out[index] = 1; //1f32
-                        }
-                        else if(i == 1){
-                                X_out[index] = i;//r32 ind 
-                        }else{
-                                float f_t = (2*M_PI*ind*i)/f; //Ft(j)=2πjt/f.
-                                if (i % 2 == 0){
-                                       X_out[index] = sin(f_t);  
+                                //This correspond to the first index of each x_t, which should always be 1
+                                X_out[cur_ind] = 1;
+                        }else if (i == 1)
+                        {
+                                //This correspond the second index of each x_t, which should be the date, t
+                                X_out[cur_ind] = t; 
+                        }else
+                        {
+                                //Calculates Ft(j)=2πjt/f
+                                float F = (2*M_PI*i*t)/f;  
+                                if(i% 2 == 0){
+                                        X_out[cur_ind] = sin(F);
                                 }else{
-                                    X_out[index] = cos(f_t);
-                                }
-                        }
+                                        X_out[cur_ind] = cos(F);
+                                } 
+                        }                       
                 }
-                
         }
         
 }
 
 // Filtered Matrix - Matrix Multiplication
 // X is assumed to be a nxm matrix
+//Output is nxn matrix
 void seq_mmMulFilt(float* X, float* y, float* X_sqr, int n, int m){
         // Calculates X * X^T
         for (int i = 0; i < n; i++)
@@ -60,19 +68,66 @@ void seq_mmMulFilt(float* X, float* y, float* X_sqr, int n, int m){
         }
 }
                 
+void seq_scatter(float* X, int* I, float* D, int n){
 
+}
+
+void seq_gauss_jordan(float* A, int n, int m){
+        for (int i = 0; i < n; i++)
+        {
+                int v1 = A[i]; 
+                float _A[m]; 
+                for (int ind = 0; ind < m; ind++)
+                {
+                        int k = ind/m; 
+                        int j = ind % m; 
+                        if(v1 == 0.0){
+                                _A[ind] = A[k*m+j]; //er ikke sikker på om det skal være det
+                        }else
+                        {
+                                float x = A[j]/v1; 
+                                if(k < n -1){
+                                        _A[ind] = A[(k+1)*m+j] - A[(k+1)*m+i] * x; 
+                                }else
+                                {
+                                        _A[ind] = x; 
+                                }
+                                
+                        }
+                        
+                }
+                
+        }
+        //in  scatter A (iota nm) A'
+        
+}
 // We need to invert here
 // Allocate the [K][2K] array in here
+// X_sqr is a nxn matrix 
 // X_inv is the output
-void seq_matInv (float* X_sqr, float* X_inv, int n, int m){
-        for (int row = 0; row < row; row++)
+void seq_matInv (float* X_sqr, float* X_inv, int n){
+        int m = 2*n; 
+        int nm = n*m; 
+        float Ap[nm]; 
+        for (int ind = 0; ind < nm; ind++)
         {
-                for (int col = 0; col < m; col ++)
+                int i = ind/m; 
+                int j = ind % m; 
+                if(j < n){
+                        int index = i* n+ j; //ved ikke om det er rigtigt
+                        Ap[ind] = X_sqr[index];
+                }else if (j == n + i)
                 {
-                        float val = X_sqr[row*n + col];
-                        X_inv[col*m + row] = val;                                               
-                }
+                        Ap[ind] = 1.0; 
+                }else
+                {
+                        Ap[ind] = 0.0
+                }    
         }
+
+        //create a gauss_jordan on Ap
+        
+    
 }
 
 
@@ -95,6 +150,7 @@ void seq_mvMulFilt(float* X, float* y, float* Beta_zero, int n, int m){
                 }                
         }       
 }
+
 
 // UnFiltered matrix * vector multiplication
 // Used for calculating Beta, and y_preds
