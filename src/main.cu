0533da4df303d617caa33ec41e681812f0872760
wbr220 <christian@cpj.nu>
2020-10-28 17:32:31 +0100

    Main

--- src/main.cu
+++ src/main.cu
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include "helpers.cu.h"
 #include "cuda-kernels.cu.h"
+//#include "cpu-kernels.cu.h"
 
 int validate(){
     return 0;
@@ -95,6 +96,65 @@ int readDataset(char* pathname, dataset* ds){
     return 0;
 }
 
+int validate(dataset* ds){
+    // KERNEL 1
+    // Make interpolation matrix
+    printf("Creating X matrix\n");
+    int k2p2 = 2*ds->k + 2;
+    int k2p2_ = (trend > 0) ? k2p2 : k2p2-1;
+    float* X_host = (float*) malloc(k2p2_ * ds->N * sizeof(float));
+    seq_mkX(k2p2_, ds->N, ds->freq, X_host);
+
+    printf("Transposing matrices and extracting Historical data\n");
+    float* Xh_host = (float*) malloc(k2p2_ * ds->n * sizeof(float));
+    float* Xth_host= (float*) malloc(k2p2_ * ds->n * sizeof(float));
+    float* Yh_host = (float*) malloc(ds->m * ds->n * sizeof(float));
+    // Do the list slicing sequentially
+    for(int k = 0; k < k2p2_; k++){
+        for (int i = 0; i < ds->n; i++){
+            // Copy Xh[:,:n]
+            Xh_host[k*ds->n + i] = X_host[k*ds->N + i];
+        }
+    }
+    for(int j = 0; j < ds->m; j++){
+        for (int i = 0; i < ds->n; i++){
+            // Copy Yh[:,:n]
+            Yh_host[j*ds->n + i] = ds->images[j*ds->N + i];
+        }
+    }
+    // Transpose X
+    seq_transpose(Xh_host, Xth_host, k2p2_, ds->n);
+    printf("[!]K1 done\n");
+
+    // KERNEL 2
+    printf("Creating Xsqr\n");
+    float* Xsqr_host = (float*) malloc(k2p2_ * k2p2_ * sizeof(float));
+    seq_mmMulFilt(Xh_host, Xth_host, Yh_host, Xsqr_host, ds->n, k2p2_);
+    printf("[!]K2 Done\n");
+    // KERNEL 3
+    printf("Inverting Xsqr\n");
+    float* Xinv_host = (float*) malloc(k2p2_ * k2p2_ * sizeof(float));
+    seq_matInv(Xsqr_host, Xinv_host, k2p2_);
+    printf("[!]K3 Done\n");
+
+    // Kernel 4
+    printf("running Vector Multiplication and calculating betas\n");
+    printf("Filtered first\n");
+    float* beta0_host = (float*) malloc(k2p2_ * sizeof(float));
+    sec_mvMulFilt(Xh_host, Yh_host, beta0_host, k2p2, ds->n);
+    printf("Unfiltered beta and y_preds\n");
+    float* beta_host = (float*) malloc(k2p2_ * sizeof(float));
+    seq_mvMul(Xinv_host, beta0_host, K, ds->n);
+    float* ypreds_host = (float*) malloc(ds->N * sizeof(float));
+    // Transpose X
+    float* Xt_host = (float*) malloc(ds->N * k2p2_ * sizeof(float));
+    seq_transpose(X_host, Xt_host, k2p2_, ds->N);
+    seq_mvMul(Xt_host, beta, K, ds->N);
+    
+
+}
+
+
 int main(int argc, char* argv[]){
     dataset* ds = (dataset*) malloc(sizeof(dataset));
     char* dsPath = "data/peru.clean";
@@ -103,4 +163,3 @@ int main(int argc, char* argv[]){
             ds->m);
     return 0;
 }
-
